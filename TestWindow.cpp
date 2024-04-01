8a4705506171b608f660a5b1518567d6723ce8c4
Maëlian Vuillemenot <vuillemenotmaelian@gmail.com>
2024-04-01 02:39:15 +0200

    edited unit test

--- TestWindow.cpp
+++ TestWindow.cpp
@@ -4,13 +4,13 @@
 
 void testWindowCreation() {
     Window window;
-    // Vérifie si la fenêtre est correctement initialisée
+   
     assert(window.GetWindow() != nullptr);
     assert(window.GetRenderer() != nullptr);
 }
 
 int main() {
-    // Exécuter le test
+   
     testWindowCreation();
     std::cout << "Window creation test passed!" << std::endl;
     return 0;
